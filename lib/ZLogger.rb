aae361815dea20cb3fca87aede1be2674d280570
zhgchgli <zhgchgli@gmail.com>
2022-08-14 00:52:11 +0800

    update error handle

--- lib/ZLogger.rb
+++ lib/ZLogger.rb
@@ -12,22 +12,16 @@ class ZLogger
 
     def logError(message)
         result = "#{caller[0]}: #{message}"
-
-        puts "Error: #{result}"
         logger.error(result)
     end
 
     def logWarn(message)
         result = "#{caller[0]}: #{message}"
-        
-        puts "Warning: #{result}"
         logger.warn(result)
     end
 
     def logInfo(message)
         result = "#{caller[0]}: #{message}"
-        
-        puts "Info: #{result}"
         logger.info(result)
     end
 end
